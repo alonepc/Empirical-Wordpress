3b1ca4003ea05dec648c36ceb4a96f7062e56233
Matthew Hall <matt@randomshapes.com>
2014-10-09 14:19:50 -0700

    Custom user name function because Buddypress doesn't have one

--- wp-content/themes/quill/buddypress/activity/entry.php
+++ wp-content/themes/quill/buddypress/activity/entry.php
@@ -10,94 +10,58 @@
  * @subpackage bp-legacy
  */
 
-global $bp;
+global $bp, $wpdb;
+
 if ( bp_activity_has_content() ) {
+
+$bp->avatar->thumb->width = 75;
+$bp->avatar->thumb->width = 75;
+
+$activity_id = bp_get_activity_id();
+$group_id = $wpdb->get_var( "SELECT item_id FROM wp_bp_activity WHERE id=$activity_id" );
+$group_id = intval($group_id);
+if($group_id === 0) {
+	$link = bp_get_activity_user_link();
+	$avatar = bp_get_activity_avatar();
+	$user_id = $wpdb->get_var( "SELECT user_id FROM wp_bp_activity WHERE id=$activity_id" );
+	$user_data = get_userdata($user_id);
+	$name = $user_data->first_name . ' ' . $user_data->last_name;
+} else {
+	$avatar = bp_core_fetch_avatar( array( 'item_id' => $group_id, 'object' => 'group', 'width' => 75, 'height' => 75 ) );
+	$link = '/groups/' . $wpdb->get_var( "SELECT slug FROM wp_bp_groups WHERE id=$group_id" );
+	$name = $wpdb->get_var( "SELECT name FROM wp_bp_groups WHERE id=$group_id" );
+}
+
 ?>
 
 <?php do_action( 'bp_before_activity_entry' ); ?>
 
 <li class="activity activity_update activity-item" id="activity-<?php bp_activity_id(); ?>">
+
 	<div class="activity-avatar">
-		<a href="<?php bp_activity_user_link(); ?>">
-			<?php
-			global $wpdb;
-			$activity_id = bp_get_activity_id();
-			$group_id = $wpdb->get_var( "SELECT item_id FROM wp_bp_activity WHERE id=$activity_id" );
-			$group_id = intval($group_id);
-			if($group_id === 0) {
-				bp_activity_avatar();
-			} else {
-				$avatar = bp_core_fetch_avatar( array( 'item_id' => $group_id, 'object' => 'group', 'width' => 75, 'height' => 75 ) );
-				echo($avatar);
-			}
-			?>
+		<a href="<?php echo($link); ?>">
+			<?php echo($avatar);  ?>
 		</a>
 	</div>
 
 	<div class="activity-content">
 
 		<div class="activity-header">
-
-			<?php
-				if($group_id == 0) {
-				
-					echo($bp->displayed_user->fullname);
-					
-				} else {
-				
-					$group_name = $wpdb->get_var( "SELECT name FROM wp_bp_groups WHERE id=$group_id" );
-					echo($group_name);
-					
-				}
-			?>
+			<a href="<?php echo($link); ?>">
+				<?php echo($name); ?>
+			</a>
 		</div>
 
 		
 
-			<div class="activity-inner">
-			
-					<?php bp_activity_content_body(); ?>
-				
-			</div>
+		<div class="activity-inner">
+			<?php bp_activity_content_body(); ?>
+		</div>
 
 
 		<?php do_action( 'bp_activity_entry_content' ); ?>
 
 	</div>
-
-	<?php do_action( 'bp_before_activity_entry_comments' ); ?>
-
-	<?php if ( ( is_user_logged_in() && bp_activity_can_comment() ) || bp_is_single_activity() ) : ?>
-
-		<div class="activity-comments">
-
-			<?php bp_activity_comments(); ?>
-
-			<?php if ( is_user_logged_in() ) : ?>
-
-				<form action="<?php bp_activity_comment_form_action(); ?>" method="post" id="ac-form-<?php bp_activity_id(); ?>" class="ac-form"<?php bp_activity_comment_form_nojs_display(); ?>>
-					<div class="ac-reply-avatar"><?php bp_loggedin_user_avatar( 'width=' . BP_AVATAR_THUMB_WIDTH . '&height=' . BP_AVATAR_THUMB_HEIGHT ); ?></div>
-					<div class="ac-reply-content">
-						<div class="ac-textarea">
-							<textarea id="ac-input-<?php bp_activity_id(); ?>" class="ac-input" name="ac_input_<?php bp_activity_id(); ?>"></textarea>
-						</div>
-						<input type="submit" name="ac_form_submit" value="<?php esc_attr_e( 'Post', 'buddypress' ); ?>" /> &nbsp; <a href="#" class="ac-reply-cancel"><?php _e( 'Cancel', 'buddypress' ); ?></a>
-						<input type="hidden" name="comment_form_id" value="<?php bp_activity_id(); ?>" />
-					</div>
-
-					<?php do_action( 'bp_activity_entry_comments' ); ?>
-
-					<?php wp_nonce_field( 'new_activity_comment', '_wpnonce_new_activity_comment' ); ?>
-
-				</form>
-
-			<?php endif; ?>
-
-		</div>
-
-	<?php endif; ?>
-
-	<?php do_action( 'bp_after_activity_entry_comments' ); ?>
 
 </li>
 
