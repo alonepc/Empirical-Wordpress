4348133bc8d801327c90a99dd03c91d7cb178ff5
Matthew Hall <matt@randomshapes.com>
2014-12-04 11:52:13 -0800

    New signup page

---
+++ wp-content/themes/quill/template-signup.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Template Name: Signup Page
+*/
+get_header();
+?>
+
+<div id="main-content" class="main-content signup-page">
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+		
+			<div class="signup-wrap">
+				<div class="row">
+					<a class="signup signup-student" href="http://www.quill.org/account/new"></a>
+					<a class="signup signup-teacher" href="http://www.quill.org/account/new"></a>
+				</div>
+				<div class="row">
+					<a class="signup signup-parent" href="http://www.quill.org/account/new"></a>
+					<a class="signup signup-contributor" href="/register"></a>
+				</div>
+			</div>
+		
+		</div>
+	</div>
+</div>
+
+<?php get_footer();?>
