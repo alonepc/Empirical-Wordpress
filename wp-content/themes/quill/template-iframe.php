0adade5fc83722e2e28702d350f1e2f3e8ea3775
Matthew Hall <matt@randomshapes.com>
2015-01-19 15:51:59 -0800

    PHP loop fix

--- wp-content/themes/quill/template-iframe.php
+++ wp-content/themes/quill/template-iframe.php
@@ -8,7 +8,11 @@ get_header();
 <div id="main-content" class="main-content signup-page">
 	<div id="primary" class="content-area">
 		<div id="content" role="main">
-			<?php the_content() ?>
+			<?php 
+			while ( have_posts() ) : the_post();
+				the_content();
+			endwhile;
+			?>
 		</div>
 	</div>
 </div>
