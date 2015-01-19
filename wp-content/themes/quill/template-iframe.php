05adc02f3808ab92a958aff29145a8ad339abcd6
Matthew Hall <matt@randomshapes.com>
2015-01-19 15:50:41 -0800

    Simple blank page template

---
+++ wp-content/themes/quill/template-iframe.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Template Name: iFrame Page
+*/
+get_header();
+?>
+
+<div id="main-content" class="main-content signup-page">
+	<div id="primary" class="content-area">
+		<div id="content" role="main">
+			<?php the_content() ?>
+		</div>
+	</div>
+</div>
+
+<?php get_footer();?>
