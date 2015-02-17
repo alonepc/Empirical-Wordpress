81bf468493aee4744f88455b73f95de6eaab095c
Matthew Hall <matt@randomshapes.com>
2015-02-16 21:21:58 -0800

    making header link go home

--- wp-content/themes/quill/header.php
+++ wp-content/themes/quill/header.php
@@ -47,7 +47,7 @@
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
 		<div class="header-main">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<h1 class="site-title"><a href="http://quill.org" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
 			<nav id="primary-navigation" class="site-navigation primary-navigation" role="navigation">			
 				
